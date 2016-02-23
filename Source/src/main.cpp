564e612827f2b3c733a80d212dc3a6c6daae0b97
nikhily <nikhil.yadala@gmail.com>
2016-02-23 19:09:55 +0530

    changing main.cpp for calling class_coverting function

--- Source/src/main.cpp
+++ Source/src/main.cpp
@@ -44,7 +44,9 @@ int main(int argc, char *argv[])
 	source_code input_code(input_src);
 	input_code.file_name=f;
 	input_code.functionize();
+	input_code.make_classes();
 	input_code.generate_cpp_code();
+	input_code.genrated_cpp_classes();
 	input_code.render_code();
 
 
