490f281703040c76b5ce65718bc3aedff90e5fa4
nikhily <nikhil.yadala@gmail.com>
2016-02-23 18:55:43 +0530

    stores the entire info of a cpp class which should be used for rendering cpp code

---
+++ Source/Header/include/class_struct.h
@@ -0,0 +1,15 @@
+//this file defines the structure for storing the classes.
+
+
+#ifndef class_struct_def
+#define class_struct_def
+
+struct class_declaration{
+	std::string name;
+	std::int num_base_func;//stores num of base functions (inheritance)
+    std::vector< std::string > base_functions;
+
+};
+
+
+#endif
