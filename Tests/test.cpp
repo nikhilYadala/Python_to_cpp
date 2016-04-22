dc1b9760d4c16addaa714d427ee30190b912af2d
mehulsawarkar <mehulsawarkar@gmail.com>
2016-04-22 19:20:59 +0530

    Testcases United

---
+++ Tests/test.cpp
@@ -0,0 +1,12 @@
+#include <limits.h>
+#include "../Source/Header/supporting_libs/supporting_libs.h"
+#include "gtest/gtest.h"
+#include<string>
+#include "test1.cpp"
+#include "functionize/test2.cpp"
+#include "generating_cpp/test3.cpp"
+
+int main(int argc, char **argv) {
+::testing::InitGoogleTest(&argc, argv);
+return RUN_ALL_TESTS();
+}
